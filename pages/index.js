7541fc58fcfc6571f8704ea4363e4413f591593c
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2026-01-16 17:22:00 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Nesse ano de 2026 inicio uma nova etapa da minha vida; vou cursar ADS na
-      Fiap, que sejam 2 anos de muito aprendizado, desenvolvimento e de muitas
-      oportunidades.
-    </h1>
-  );
+  return <h1>Um 2026 excelente!</h1>;
 }
 
 export default Home;
